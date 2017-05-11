4480defdcd867da98457e744c787308d0ad470d9
rohscx <R_E__g_@Macintosh.local>
2017-05-11 10:46:09 -0400

    existCheck

--- mysqli.php
+++ mysqli.php
@@ -92,6 +92,7 @@ class mysqlquery {
 		$this->sqlquery($this->query_6, $this->int_1);	// checks for MAC Address
 		//echo "existCheck result " . $this->results;
 		print "existCheck result " . $this->results[0]['State'] . "\n";	// debug
+		print "existCheck result " . $this->results[0]['Mac_ID'] . "\n";	// debug
 		if (isset($this->results[0]['Fname']) && isset($this->results[0]['Lname'])) {
 			$this->sqlquery($this->query_6, $this->int_1);
 		} else {
