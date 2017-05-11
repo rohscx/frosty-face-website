2df559b31709b7cb8e157fc633dd1bee3e10fdfb
rohscx <R_E__g_@Macintosh.local>
2017-05-11 10:30:37 -0400

    query_7
    put $query_6 back the way it was, modified $query_7 to provide more
    specific information

--- mysqli.php
+++ mysqli.php
@@ -41,7 +41,7 @@ class mysqlquery {
 				ON am.Mac_ID = amm.Mac_ID
 				WHERE am.Valid_Until = '1000-01-01 00:00:00' AND   CONCAT( Fname,  ' ', Lname ) LIKE  ?
 				ORDER BY am.Valid_From ASC";	//search MAB table DB by First or Last name
-	protected $query_6 = "SELECT am.Mac_ID, au.User_ID, au.Fname, au.Lname, a.ACA_Name, a.ACA_Bname, am.Valid_From, am.State, amm.Action, am.Ticket
+	protected $query_6 = "SELECT am.Mac_ID, au.Fname, au.Lname, a.ACA_Name, a.ACA_Bname, am.Valid_From, am.State, amm.Action, am.Ticket
 				FROM aca_mab as am
 				JOIN aca_user as au
 				USING (User_ID)
@@ -51,6 +51,16 @@ class mysqlquery {
 				ON am.Mac_ID = amm.Mac_ID
 				WHERE am.Valid_Until = '1000-01-01 00:00:00' AND  am.Mac_ID LIKE  ?
 				ORDER BY am.Valid_From ASC";	// search MAB table DB by MAC Address and Returns sigle row
+	protected $query_7 = "SELECT am.Mac_ID, au.Fname, au.Lname, a.ACA_Name, a.ACA_Bname, am.Valid_From, am.State, amm.Action, am.Ticket
+				FROM aca_mab as am
+				JOIN aca_user as au
+				USING (User_ID)
+				JOIN aca as a
+				ON a.Aca_ID = au.Aca_ID
+				JOIN aca_mab_metadata as amm
+				ON am.Mac_ID = amm.Mac_ID
+				WHERE am.Valid_Until = '1000-01-01 00:00:00' AND   CONCAT( Fname,  ' ', Lname ) =  ?
+				ORDER BY am.Valid_From ASC";	// search MAB table DB by First or Last name
 	protected $results;
 
   	function __construct($sqlQuery,$sqlWhere) {
@@ -79,7 +89,7 @@ class mysqlquery {
 
   }
 	function existCheck($query, $mac) {
-		$this->sqlquery($this->query_6, $this->int_1);
+		$this->sqlquery($this->query_7, $this->int_1);	// checks for username
 		//echo "existCheck result " . $this->results;
 		print "existCheck result " . $this->results[0]['State'] . "\n";	// debug
 		if (isset($this->results[0]['Fname']) && isset($this->results[0]['Lname'])) {
