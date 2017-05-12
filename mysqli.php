bdb08866631b0dd9e84afd82a052d98918b6ace2
rohscx <R_E__g_@Macintosh.local>
2017-05-12 16:53:31 -0400

    if (isset($_GET

--- mysqli.php
+++ mysqli.php
@@ -103,8 +103,8 @@ class mysqlquery {
 	function existCheck($Query) {
 		$temp_type = $this->a_param_type;
 		$temp_bind = $this->a_bind_params;
-		$this->a_param_type = array($this->a_param_type[0]);
-		$this->a_bind_params = array($this->a_bind_params[2]);	// this is for testing
+		$this->a_param_type = array($this->a_param_type[1]);
+		$this->a_bind_params = array($this->a_bind_params[3]);	// this is for testing
 		$this->sqlquery($this->query_8);	// checks for MAC Address
 
 		//echo "existCheck result " . $this->results;
