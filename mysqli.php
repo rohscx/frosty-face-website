fdc7a355f27d16101b5307be1e1786c27a8a94cd
rohscx <R_E__g_@Macintosh.local>
2017-05-15 14:27:50 -0400

    function sqlquery

--- mysqli.php
+++ mysqli.php
@@ -189,7 +189,7 @@ class mysqlquery {
 	  $x = 0;
 	  $meta = $stmt->result_metadata();
 	  $parameters = array();
-  if (isset($meta)) {
+  if ($meta) {
 		while($field = $meta->fetch_field()) {
     	$parameters[] = &$row[$field->name];
 		}
