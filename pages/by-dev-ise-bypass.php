a2c3f5e32dc8ee70e6cbf5e4259d0b7dbced31d1
rohscx <R_E__g_@Macintosh.local>
2017-05-17 18:16:34 -0400

    debug

--- pages/by-dev-ise-bypass.php
+++ pages/by-dev-ise-bypass.php
@@ -60,9 +60,11 @@
             if (this.readyState == 4 && this.status == 200) {
                 myObj = JSON.parse(this.responseText);
                 document.getElementById(thediv).innerHTML =  myObj.Type.fontcolor("green")  + " : " + myObj.Normalized + "<br>" + myObj.Encoded;
+/*
                 if (myObj.Type == "MAC") {
                   document.getElementById('mac_1').value =  myObj.Normalized;
                 }
+                */
             }
         }
     xmlhttp.open('GET', thefile+'?'+thekey+'='+document.form_1.mac_1.value, true);
