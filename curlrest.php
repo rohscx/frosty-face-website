27d33cc64d7df913b7c9b0b2f8cf3a4c96c8e721
rohscx <rohscx@gmail.com>
2017-05-19 11:11:44 -0400

    Update curlrest.php

--- curlrest.php
+++ curlrest.php
@@ -33,14 +33,14 @@ class curlauth {
     return $this->$name = $value;
   }	// used to set properties
   function myCurl() {
-	/*
+	
     	echo "curlAddress myCurl  ::" . $this->curlAddress . "<br>";	// debug
 	echo "curlData myCurl  ::" .  $this->curlData . "<br>";	// debug
 	echo "curlCustom myCurl  ::" .  $this->curlCustom . "<br>";	// debug
 	echo "curlPost myCurl  ::" .   $this->curlPost . "<br>";	// debug
 	echo "curlHTTP myCurl  ::" .   print_r($this->curlHTTP) . "<br>";	// debug
 	echo "FULLLcurlAddress myCurl  ::" .   $this->curlAddress . $this->curlData . "<br>"; // debug
-	*/
+	
 	$curl = curl_init();
 	curl_setopt_array($curl, array(
 		CURLOPT_SSL_VERIFYPEER => false,    // disables ssl server cert verify check
@@ -186,7 +186,7 @@ function iseCurl_1() {
 		}
 		//$this->curlHTTP = array($arr['serviceTicket']);
 		//print_r($arr);	// debug
-		print_r($this->curlHTTP);	// debug
+		//print_r($this->curlHTTP);	// debug
 	}
 	function primeTicket_1(){
    		$auth_1 ="B1@ck_Sn@k3_M0@n"; 	// populate with a ticket
