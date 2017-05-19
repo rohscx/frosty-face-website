0e6b8ea6ce9d6bfd0a49ccafb1199151a1f287e9
rohscx <rohscx@gmail.com>
2017-05-19 14:37:36 -0400

    Update curlrest.php

--- curlrest.php
+++ curlrest.php
@@ -63,7 +63,8 @@ class curlauth {
     $response_info=curl_getinfo($curl);
     $err = curl_error($curl);	    
     curl_close($curl);
-    if ($err || $response_info['http_code'] == "500") {
+    print $response_info['http_code'];
+    if ($err || $response_info['http_code'] == 500) {
         echo "cURL Error #:" . $err;
 	echo "RESPONSE HTTP STATUS CODES  " . print_r( $aaa) . "<br />";	// debug
     } else {
