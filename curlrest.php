fc89d29666a8e139bc64ea475138a13b9a47d5c1
rohscx <rohscx@gmail.com>
2017-05-19 14:57:42 -0400

    Update curlrest.php

--- curlrest.php
+++ curlrest.php
@@ -63,7 +63,7 @@ class curlauth {
     $response_info=curl_getinfo($curl);
     $err = curl_error($curl);	    
     curl_close($curl);
-    print $response_info['http_code'];
+    //print $response_info['http_code'];	// debug
     if ($err) {
         echo "cURL Error #:" . $err;
 	//echo "RESPONSE HTTP STATUS CODES  " . print_r( $response_info) . "<br />";	// debug
