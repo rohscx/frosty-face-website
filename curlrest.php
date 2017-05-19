34f1d08157ee4d8894139a3a696ecd6ebe176144
rohscx <rohscx@gmail.com>
2017-05-19 09:26:34 -0400

    Update curlrest.php

--- curlrest.php
+++ curlrest.php
@@ -239,7 +239,7 @@ if (isset($_GET['Type']) & isset($_GET['curlAddress']) & isset($_GET['curlData']
 	//echo $_GET['curlAddress'] . "<br />";	// debug
 	//echo $_GET['curlData'] . "<br />";	// debug
 	if (strpos($_GET['curlAddress'], "https://agaisepr01.fpicore.fpir.pvt:9060/ers/config/endpoint") !== false) {
-    		$a->curlPost = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\r\n<ns3:endpoint name=\"name\" id=\"id\" description=\"description\"\r\nxmlns:ns2=\"ers.ise.cisco.com\" xmlns:ns3=\"identity.ers.ise.cisco.com\">\r\n<groupId>c07b4190-247b-11e7-b9de-00505698708f</groupId>\r\n<identityStore></identityStore>\r\n<identityStoreId></identityStoreId>\r\n<mac>1D:1A:7E:44:3A:9A</mac>\r\n<portalUser></portalUser>\r\n<profileId></profileId>\r\n<staticGroupAssignment>true</staticGroupAssignment>\r\n<staticProfileAssignment>false</staticProfileAssignment>\r\n</ns3:endpoint>\r\n";
+    		$a->curlPost = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\r\n<ns3:endpoint name=\"name\" id=\"id\" description=\"description\"\r\nxmlns:ns2=\"ers.ise.cisco.com\" xmlns:ns3=\"identity.ers.ise.cisco.com\">\r\n<groupId>c07b4190-247b-11e7-b9de-00505698708f</groupId>\r\n<identityStore></identityStore>\r\n<identityStoreId></identityStoreId>\r\n<mac>" . $_GET['curlData'] . "</mac>\r\n<portalUser></portalUser>\r\n<profileId></profileId>\r\n<staticGroupAssignment>true</staticGroupAssignment>\r\n<staticProfileAssignment>false</staticProfileAssignment>\r\n</ns3:endpoint>\r\n";
 	} else {
 		$a->curlPost = $_GET['curlPost'];
 	}
@@ -251,8 +251,13 @@ if (isset($_GET['Type']) & isset($_GET['curlAddress']) & isset($_GET['curlData']
 		$a->curlData = "(" . $_GET['curlData'] . ")";	// formats user input
 		$a->primeCurl_1();	// calls the correct function based on the GET type
 	} elseif ($_GET['Type'] == "iseTicket_1") {
-		$a->curlData = $_GET['curlData'];	// formats user input
-		$a->iseCurl_1();	// calls the correct function based on the GET Type
+		if (strpos($_GET['curlAddress'], "https://agaisepr01.fpicore.fpir.pvt:9060/ers/config/endpoint") !== false) {
+			$a->curlData = "";	// formats user input
+			$a->iseCurl_1();	// calls the correct function based on the GET Type
+		} else {
+			$a->curlData = $_GET['curlData'];	// formats user input
+			$a->iseCurl_1();	// calls the correct function based on the GET Type
+		}
 	} elseif ($_GET['Type'] == "apicTicket_1") {
 		$a->curlData = $_GET['curlData'];	// formats user input
 		$a->apicCurl_1();	// calls the correct function based on the GET tpe
