68c0cd28faffc59b3e48e4c53429cad51fdb1603
rohscx <R_E__g_@Macintosh.local>
2017-05-24 12:26:51 -0400

    function curlreturn_2

--- pages/by-dev-ise-bypass.php
+++ pages/by-dev-ise-bypass.php
@@ -326,7 +326,6 @@
               }
               var replacement_1 = "/" + myObj.resources.resource['@attributes'].id;
               var replacement_2 = "PUT";
-              var array = [replacement_1: 89, replacement_2: 153];
               thedata_2 = thedata_2.replaceAt(89,replacement_1);
               thedata_2 = thedata_2.replaceAt(153,replacement_2);
               alert(thedata_2);
