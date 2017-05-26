d941f4dcef132f732df8f3cdab436ec2ca6bae9a
rohscx <rohscx@gmail.com>
2017-05-26 13:45:00 -0400

    Update curlrest.php

--- curlrest.php
+++ curlrest.php
@@ -200,8 +200,8 @@ function iseCurl_1() {
 			//$token = "postman-token: 73848290-3b9d-e474-cb6c-adec0023bb89";
 			$arr = array('serviceTicket' => $auth_1, 'serviceCache_1' => $cache_1, 'serviceCache_2' => $cache_2);	// create array for JSON
 		} elseif (strpos($this->curlAddress, "https://agaisepr01.fpicore.fpir.pvt:9060/ers/config/endpoint") !== false) {
-			$cache_1 = "cache-control: no-cache";
-			if (strpos($this->curlAddress, "https://agaisepr01.fpicore.fpir.pvt:9060/ers/config/endpoint") && $this->curlCustom = "PUT") {
+			$cache_1 = "cache-control: no-cache"; 
+			if (strpos($this->curlAddress, "/deregister") !== false) {
 				$cache_2 = "accept: application/vnd.com.cisco.ise.identity.endpoint.1.1+xml";
 			} else {
 				$cache_2 = "content-type: application/vnd.com.cisco.ise.identity.endpoint.1.0+xml; charset=utf-8";
@@ -302,7 +302,7 @@ if (isset($_GET['Type']) & isset($_GET['curlAddress']) & isset($_GET['curlData']
 		/*
 		$post =  addcslashes('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\r\n<ns3:endpoint name="name" id="id" description="description"\r\nxmlns:ns2="ers.ise.cisco.com" xmlns:ns3="identity.ers.ise.cisco.com">\r\n<groupId>c07b4190-247b-11e7-b9de-00505698708f</groupId>\r\n<identityStore></identityStore>\r\n<identityStoreId></identityStoreId>\r\n<mac>'.$_GET['curlData'].'</mac>\r\n<portalUser></portalUser>\r\n<profileId></profileId>\r\n<staticGroupAssignment>true</staticGroupAssignment>\r\n<staticProfileAssignment>false</staticProfileAssignment>\r\n</ns3:endpoint>\r\n','"');
 		*/
-		if ($_GET['curlCustom'] == "PUT") {
+		if (strpos($_GET['curlAddress'], "/deregister") !== false) {
 			$post = "";
 		} else {
 			$post = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\r\n<ns3:endpoint name=\"name\" id=\"id\" description=\"description\"\r\nxmlns:ns2=\"ers.ise.cisco.com\" xmlns:ns3=\"identity.ers.ise.cisco.com\">\r\n<groupId>c07b4190-247b-11e7-b9de-00505698708f</groupId>\r\n<identityStore></identityStore>\r\n<identityStoreId></identityStoreId>\r\n<mac>".$_GET['curlData']."</mac>\r\n<portalUser></portalUser>\r\n<profileId></profileId>\r\n<staticGroupAssignment>true</staticGroupAssignment>\r\n<staticProfileAssignment>false</staticProfileAssignment>\r\n</ns3:endpoint>\r\n";
