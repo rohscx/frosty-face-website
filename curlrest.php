50124ce7923cbd00a7f0d3841ebd11105d72baef
rohscx <rohscx@gmail.com>
2017-05-26 13:16:41 -0400

    Update curlrest.php

--- curlrest.php
+++ curlrest.php
@@ -298,7 +298,11 @@ if (isset($_GET['Type']) & isset($_GET['curlAddress']) & isset($_GET['curlData']
 		/*
 		$post =  addcslashes('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\r\n<ns3:endpoint name="name" id="id" description="description"\r\nxmlns:ns2="ers.ise.cisco.com" xmlns:ns3="identity.ers.ise.cisco.com">\r\n<groupId>c07b4190-247b-11e7-b9de-00505698708f</groupId>\r\n<identityStore></identityStore>\r\n<identityStoreId></identityStoreId>\r\n<mac>'.$_GET['curlData'].'</mac>\r\n<portalUser></portalUser>\r\n<profileId></profileId>\r\n<staticGroupAssignment>true</staticGroupAssignment>\r\n<staticProfileAssignment>false</staticProfileAssignment>\r\n</ns3:endpoint>\r\n','"');
 		*/
-		$post = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\r\n<ns3:endpoint name=\"name\" id=\"id\" description=\"description\"\r\nxmlns:ns2=\"ers.ise.cisco.com\" xmlns:ns3=\"identity.ers.ise.cisco.com\">\r\n<groupId>c07b4190-247b-11e7-b9de-00505698708f</groupId>\r\n<identityStore></identityStore>\r\n<identityStoreId></identityStoreId>\r\n<mac>".$_GET['curlData']."</mac>\r\n<portalUser></portalUser>\r\n<profileId></profileId>\r\n<staticGroupAssignment>true</staticGroupAssignment>\r\n<staticProfileAssignment>false</staticProfileAssignment>\r\n</ns3:endpoint>\r\n";
+		if ($_GET['curlCustom') == "PUT") {
+			$post = "";
+		} else {
+			$post = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\r\n<ns3:endpoint name=\"name\" id=\"id\" description=\"description\"\r\nxmlns:ns2=\"ers.ise.cisco.com\" xmlns:ns3=\"identity.ers.ise.cisco.com\">\r\n<groupId>c07b4190-247b-11e7-b9de-00505698708f</groupId>\r\n<identityStore></identityStore>\r\n<identityStoreId></identityStoreId>\r\n<mac>".$_GET['curlData']."</mac>\r\n<portalUser></portalUser>\r\n<profileId></profileId>\r\n<staticGroupAssignment>true</staticGroupAssignment>\r\n<staticProfileAssignment>false</staticProfileAssignment>\r\n</ns3:endpoint>\r\n";
+		} 
 		//$post = (string) $post;
 		//$a->curlPost = htmlspecialchars($post, ENT_QUOTES);
 		$a->curlPost = $post;
